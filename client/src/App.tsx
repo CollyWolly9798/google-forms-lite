0299d4a0d6d1d42ad744f37cd8c18f3537eb4900
Oleksandr <collywolly9798@gmail.com>
2025-08-13 02:04:28 +0200

    add lazy load on pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage.tsx';
-import FormBuilder from './pages/FormBuilder/FormBuilderPage.tsx';
-import FormFiller from './pages/FormFiller/FormFillerPage.tsx';
-import FormResponsesPage from './pages/FormResponses/FormResponsesPage.tsx';
+import { lazy, Suspense } from 'react';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const FormBuilderPage = lazy(
+  () => import('./pages/FormBuilder/FormBuilderPage.tsx'),
+);
+const FormFillerPage = lazy(
+  () => import('./pages/FormFiller/FormFillerPage.tsx'),
+);
+const FormResponsesPage = lazy(
+  () => import('./pages/FormResponses/FormResponsesPage.tsx'),
+);
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/forms/new" element={<FormBuilder />} />
-      <Route path="/forms/:id/fill" element={<FormFiller />} />
-      <Route path="/forms/:id/responses" element={<FormResponsesPage />} />
-    </Routes>
+    <Suspense
+      fallback={<div className="text-center text-gray-500 mt-10">Loading</div>}
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/forms/new" element={<FormBuilderPage />} />
+        <Route path="/forms/:id/fill" element={<FormFillerPage />} />
+        <Route path="/forms/:id/responses" element={<FormResponsesPage />} />
+      </Routes>
+    </Suspense>
   );
 }
